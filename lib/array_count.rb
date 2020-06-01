fd3ea941307f8846cbf321738a3790b0ef2e4458
leonie crutchfield <leoniecrutchfield@gmail.com>
2020-06-01 12:59:19 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,29 @@
 def count_strings(array)
+i = 0
+total = 0
+
+
+while i < array.length do
+  if array[i] == "String"
+    total += 1 
+  end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   # Return the total number of strings in the provided array using the count enumerable
-end
+
 
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
-end
+  # Return the total number of EMPTY strings in the prov `ided array using the count enumerable
+end 
